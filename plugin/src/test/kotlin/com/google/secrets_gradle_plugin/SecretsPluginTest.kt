85c2ca2f1f5121f02cc73eed86ebea1d968b6feb
Erick Sumargo <ericksumargo@outlook.com>
2021-04-13 08:52:23 +0700

    remove unused import

--- plugin/src/test/kotlin/com/google/secrets_gradle_plugin/SecretsPluginTest.kt
+++ plugin/src/test/kotlin/com/google/secrets_gradle_plugin/SecretsPluginTest.kt
@@ -14,7 +14,6 @@
 
 package com.google.secrets_gradle_plugin
 
-import com.android.build.gradle.api.ApplicationVariant
 import com.android.build.gradle.internal.core.InternalBaseVariant
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
