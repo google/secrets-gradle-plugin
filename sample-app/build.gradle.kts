99169bbd5a734146b5648394098c0b70103cca83
Chris Arriola <carriola@google.com>
2021-06-16 16:39:54 -0700

    chore: Rename maven artifact to com.google.android.libraries.mapsplatform.secrets-gradle-plugin (#23)
    * chore: Rename maven artifact to com.google.android.libraries.mapsplatform.secrets-gradle-plugin
    
    * Add note about maven coordinate change.
    
    * Prevent running release.yml

--- sample-app/build.gradle.kts
+++ sample-app/build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
     id("kotlin-android")
 
     // 1. Include the plugin
-    id("com.google.secrets_gradle_plugin") version "0.6"
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin.secrets-gradle-plugin")
 }
 
 android {
